de4194d2aac438f2fe3334eb0e519b5764dfe94f
Salam <salamhmb@gmail.com>
2019-11-11 11:39:11 +0100

    Add Admin Model and Migration

---
+++ database/seeds/AdminsTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use App\Models\Admin;
+use Illuminate\Database\Seeder;
+
+class AdminsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Admin::create([
+            'name' => 'Salam',
+            'email' => 'a@a.aa',
+            'password' => bcrypt('aaaaaaaa'),
+        ]);
+    }
+}
